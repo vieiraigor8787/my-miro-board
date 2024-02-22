5eef376c2337202b289c3bee8bea0eb5966748d2
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2024-02-22 10:10:35 -0300

    setting navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,48 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+'use client'
+
+import {
+  OrganizationSwitcher,
+  UserButton,
+  useOrganization,
+} from '@clerk/nextjs'
+
+import { SearchInput } from './search-input'
+import { InviteButton } from './invite-button'
 
 export const Navbar = () => {
+  const { organization } = useOrganization()
+
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex flex-1"></div>
+    <div className="flex items-center gap-x-4 p-5">
+      <div className="hidden lg:flex flex-1">
+        <SearchInput />
+      </div>
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                maxWidth: '376px',
+              },
+              organizationSwitcherTrigger: {
+                padding: '6px',
+                width: '100%',
+                borderRadius: '8px',
+                border: '1px solid #e5e7eb',
+                justifyContent: 'space-between',
+                backgroundColor: 'white',
+              },
+            },
+          }}
+        />
+      </div>
       <UserButton />
+      {organization && <InviteButton />}
     </div>
   )
 }
