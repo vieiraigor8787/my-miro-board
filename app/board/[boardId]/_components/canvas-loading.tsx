9540224dd8b392f6151e8372fa876ba0d89fe29c
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2024-02-28 15:24:48 -0300

    skeleton for canvas components

--- app/board/[boardId]/_components/canvas-loading.tsx
+++ app/board/[boardId]/_components/canvas-loading.tsx
@@ -1,11 +1,16 @@
 import { Loader } from 'lucide-react'
 
-import { Skeleton } from '@/components/ui/skeleton'
+import { Info } from './info'
+import { Participants } from './participants'
+import { Toolbar } from './toolbar'
 
 export const CanvasLoading = () => {
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none flex items-center justify-center">
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
+      <Info.Skeleton />
+      <Participants.Skeleton />
+      <Toolbar.Skeleton />
     </main>
   )
 }
--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -10,11 +10,13 @@ interface BoardIdPageProps {
 }
 
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
-  return (
-    <Room roomId={params.boardId} fallback={<CanvasLoading />}>
-      <Canvas boardId={params.boardId} />
-    </Room>
-  )
+  return <CanvasLoading />
+
+  // return (
+  //   <Room roomId={params.boardId} fallback={<CanvasLoading />}>
+  //     <Canvas boardId={params.boardId} />
+  //   </Room>
+  // )
 }
 
 export default BoardIdPage
