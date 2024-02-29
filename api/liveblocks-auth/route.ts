e352313e8439d07f1938c8bcad80c4dc6c888b44
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2024-02-29 17:08:12 -0300

    room authentication: fix

--- api/liveblocks-auth/route.ts
+++ api/liveblocks-auth/route.ts
@@ -1,45 +1,34 @@
-import { ConvexHttpClient } from 'convex/browser'
 import { auth, currentUser } from '@clerk/nextjs'
 import { Liveblocks } from '@liveblocks/node'
+import { ConvexHttpClient } from 'convex/browser'
 
 import { api } from '@/convex/_generated/api'
 
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 const liveblocks = new Liveblocks({
-  secret:
-    'sk_dev_Pma75TbpPaSL_rJrTGoiXclZxbbqPzQnPLt0sHiU6Bobvr_tKuueMA4Grta9Fos7',
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 })
 
 export async function POST(request: Request) {
   const authorization = await auth()
   const user = await currentUser()
 
-  console.log('auth info', { authorization, user })
-
   if (!authorization || !user) {
-    return new Response('Não autorizado', { status: 403 })
+    return new Response('Unauthorized', { status: 403 })
   }
 
   const { room } = await request.json()
   const board = await convex.query(api.board.get, { id: room })
-  console.log('auth info', {
-    room,
-    board,
-    boardOrgId: board?.orgId,
-    userOrgId: authorization.orgId,
-  })
 
   if (board?.orgId !== authorization.orgId) {
-    return new Response('Não autorizado')
+    return new Response('Unauthorized', { status: 403 })
   }
 
   const userInfo = {
-    name: user.firstName || 'Sem nome',
+    name: user.firstName || 'Teammeate',
     picture: user.imageUrl,
   }
-
-  console.log({ userInfo })
 
   const session = liveblocks.prepareSession(user.id, { userInfo })
 
@@ -48,7 +37,5 @@ export async function POST(request: Request) {
   }
 
   const { status, body } = await session.authorize()
-  console.log({ status, body }, 'allowed')
-
   return new Response(body, { status })
 }
