84d4569be8535d915bbf33a7db2faa14abdf9863
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2024-03-01 16:36:54 -0300

    cursor presence: last css adjustment

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -71,7 +71,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         onPointerMove={onPointerMove}
         onPointerLeave={onPointerLeave}
       >
-        <g>
+        <g style={{ transform: `translate(${camera.x}px, ${camera.y}px)` }}>
           <CursorsPresence />
         </g>
       </svg>
