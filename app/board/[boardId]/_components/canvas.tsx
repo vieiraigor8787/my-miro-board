7025ae3145158c4a8d7d73ae39cf12aa21e40410
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2024-03-13 12:19:54 -0300

    onLayerPointerDown

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -133,6 +133,28 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 
   const selections = useOthersMapped((other) => other.presence.selection)
 
+  const onLayerPointerDown = useMutation(
+    ({ self, setMyPresence }, e: React.PointerEvent, layerId: string) => {
+      if (
+        canvasState.mode === CanvasMode.Pencil ||
+        canvasState.mode === CanvasMode.Inserting
+      ) {
+        return
+      }
+
+      history.pause()
+      e.stopPropagation()
+
+      const point = pointerEventToCanvasPoint(e, camera)
+
+      if (!self.presence.selection.includes(layerId)) {
+        setMyPresence({ selection: [layerId] }, { addToHistory: true })
+      }
+      setCanvasState({ mode: CanvasMode.Translating, current: point })
+    },
+    [setCanvasState, camera, history, canvasState.mode]
+  )
+
   const layerIdsToColorSelection = useMemo(() => {
     const layerIdsToColorSelection: Record<string, string> = {}
 
@@ -171,7 +193,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
             <LayerPreview
               key={layerId}
               id={layerId}
-              onLayerPointDown={() => {}}
+              onLayerPointDown={onLayerPointerDown}
               selectionColor={layerIdsToColorSelection[layerId]}
             />
           ))}
