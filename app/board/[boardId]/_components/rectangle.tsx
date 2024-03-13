198350c44157eea97eba5563a22cf76c610e70b8
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2024-03-13 12:07:07 -0300

    fill property

--- app/board/[boardId]/_components/rectangle.tsx
+++ app/board/[boardId]/_components/rectangle.tsx
@@ -1,3 +1,4 @@
+import { colorToCss } from '@/lib/utils'
 import { RectangleLayer } from '@/types/canvas'
 
 interface RectangleProps {
@@ -24,8 +25,8 @@ export const Rectangle = ({
       width={width}
       height={height}
       strokeWidth={1}
-      fill="#000"
-      stroke="transparent"
+      fill={fill ? colorToCss(fill) : '#ccc'}
+      stroke={selectionColor || 'transparent'}
     />
   )
 }
